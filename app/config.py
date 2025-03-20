3e5215b8f73c8b0a26ce1f93add817f87022f5aa
Hourunze1997 <1043170898@qq.com>
2025-03-20 16:35:09 +0800

    init

--- app/config.py
+++ app/config.py
@@ -1,25 +1,22 @@
+import json
 import os
-from pydantic import Field
-from pydantic_settings import BaseSettings  # 新的导入方式
-from dotenv import load_dotenv
-
-# load_dotenv(os.path.join(os.path.dirname(__file__), '..', '.env.example'))
 
 
-class Settings(BaseSettings):
-    env: str = Field(default="development", env="APP_ENV")
-    webhook_secret: str = Field(..., env="WEBHOOK_SECRET")
-    client_id: str = Field(..., env="CLIENT_ID")
-    euler_user: str = Field(..., env="EULER_USER")
-    euler_password: str = Field(..., env="EULER_PASSWORD")
-    silicon_token: str = Field(..., env="SILICON_TOKEN")
-    gitee_token: str = Field(..., env="GITEE_TOKEN")
-    github_token: str = Field(..., env="GITHUB_TOKEN")
-    gitcode_token: str = Field(..., env="GITCODE_TOKEN")
+class Settings:
+    def __init__(self):
+        config_path_env = os.getenv('CONFIG_PATH')
+        with open(config_path_env, 'r') as config_file:
+            config = json.load(config_file)
 
-    class Config:
-        env_file_encoding = "utf-8"
-        extra = "ignore"
+            env = config.get("APP_ENV")
+            webhook_secret: str = config.get("WEBHOOK_SECRET")
+            client_id: str = config.get("CLIENT_ID")
+            euler_user: str = config.get("EULER_USER")
+            euler_password: str = config.get("EULER_PASSWORD")
+            silicon_token: str = config.get("SILICON_TOKEN")
+            gitee_token: str = config.get("GITEE_TOKEN")
+            github_token: str = config.get("GITHUB_TOKEN")
+            gitcode_token: str = config.get("GITCODE_TOKEN")
 
 
 settings = Settings()
