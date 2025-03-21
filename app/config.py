ec2d11ea45ecfdbe3881f3720b54aab5e6fae343
Hourunze1997 <1043170898@qq.com>
2025-03-21 09:19:59 +0800

    init

--- app/config.py
+++ app/config.py
@@ -4,19 +4,18 @@ import os
 
 class Settings:
     def __init__(self):
-        config_path_env = os.getenv('CONFIG_PATH')
+        config_path_env = "conf.yaml"
         with open(config_path_env, 'r') as config_file:
             config = json.load(config_file)
-
-            env = config.get("APP_ENV")
-            webhook_secret: str = config.get("WEBHOOK_SECRET")
-            client_id: str = config.get("CLIENT_ID")
-            euler_user: str = config.get("EULER_USER")
-            euler_password: str = config.get("EULER_PASSWORD")
-            silicon_token: str = config.get("SILICON_TOKEN")
-            gitee_token: str = config.get("GITEE_TOKEN")
-            github_token: str = config.get("GITHUB_TOKEN")
-            gitcode_token: str = config.get("GITCODE_TOKEN")
+            self.env = config.get("APP_ENV")
+            self.webhook_secret: str = config.get("WEBHOOK_SECRET")
+            self.client_id: str = config.get("CLIENT_ID")
+            self.euler_user: str = config.get("EULER_USER")
+            self.euler_password: str = config.get("EULER_PASSWORD")
+            self.silicon_token: str = config.get("SILICON_TOKEN")
+            self.gitee_token: str = config.get("GITEE_TOKEN")
+            self.github_token: str = config.get("GITHUB_TOKEN")
+            self.gitcode_token: str = config.get("GITCODE_TOKEN")
 
 
 settings = Settings()
